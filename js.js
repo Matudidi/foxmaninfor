9a8c8442ed97cd74515fbc9c1b89adc1a72dd844
Matudidi <matudidienock@gmail.com>
2024-06-02 21:26:50 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+
+
+window.addEventListener('scroll', ()=>{
+    document.querySelector('.nav-tri').classList.add('jscss');
+
+})
+
+document.querySelectorAll('.cont-card').forEach(chaque_card =>{
+    chaque_card.addEventListener('click', ()=>{
+        //alert('salut');
+    })
+})
