3a9ded3c68824e4d8673045ec59cc4adf1e0983f
Matudidi <matudidienock@gmail.com>
2024-06-03 23:15:00 +0200

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,25 @@
+<?php
+    $resultat;
+
+    $nb1 = 20;
+    $nb2 = 10;
+
+    $resultat =  (string) $nb1+$nb2;
+    var_dump($resultat);
+
+    $chaine = "manza";
+    var_dump($chaine);
+
+    $tableu = [
+        'l1' => ['manza', 'nsuka'],
+        'l2' => ['manza', 'nsuka']
+    ];
+
+    foreach ($tableu as $ke => $value) {
+        foreach ($tableu as $value) {
+            echo($tableu);
+            echo($value);
+        }
+        
+    }
+?>
